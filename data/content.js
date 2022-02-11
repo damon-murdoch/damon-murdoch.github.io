ef3b7edccd563dc7b504933ae5a33a4f75aef52b
Damon Murdoch <Damon.Murdoch@qed.qld.gov.au>
2022-02-11 14:57:01 +1000

    Configured home page

---
+++ data/content.js
@@ -0,0 +1,74 @@
+const PAGES = {
+  "home": {
+    "name": "Home",
+    "link": "?home",
+    "title": "Home Page", 
+    "content": `
+      <div id="text-content">
+        <p>
+          Hello and welcome to Dragapult.xyz! My name is Scrubbs and I 
+          created this website to host all of my resources related to 
+          my hobbies such as Pokémon VGC, TCG, Maximum Tune and more. 
+          These resources include a number of different software tools I
+          have created, such as a type coverage calculator and pokemon 
+          EV spread optimiser as well as my personal blog and socials.
+        </p>
+        <p>
+          In the future I am also planning on adding a database of both 
+          Pokemon VGC teams and TCG deck lists to the site, including 
+          both modern and old formats. These will primarily be from 
+          my personal collection, or submitted to me with credit by 
+          other members of the community.
+        </p>
+      </div>
+    `
+  }, 
+  "vgc": {
+    "name": "VGC",
+    "link": "?vgc",
+    "title": "Pokémon VGC", 
+    "content": `
+      <div class="d-flex m-2 bg-secondary rounded">
+        <div id="text-content">
+        
+        </div>
+      </div>
+    `
+  }, 
+  "tcg": {
+    "name": "TCG",
+    "link": "?tcg",
+    "title": "Pokémon TCG", 
+    "content": `
+      <div class="d-flex m-2 bg-secondary rounded">
+        <div id="text-content">
+
+        </div>
+      </div>
+    `
+  }, 
+  "ssb": {
+    "name": "SSB",
+    "link": "?ssb",
+    "title": "Super Smash Brothers", 
+    "content": `
+      <div class="d-flex m-2 bg-secondary rounded">
+        <div id="text-content">
+
+        </div>
+      </div>
+    `
+  }, 
+  "wmmt": {
+    "name": "WMMT",
+    "link": "?wmmt",
+    "title": "Wangan Midnight Maximum Tune", 
+    "content": `
+      <div class="d-flex m-2 bg-secondary rounded">
+        <div id="text-content">
+
+        </div>
+      </div>
+    `
+  }
+};
